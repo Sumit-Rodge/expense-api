a695cd26818287fd8f729745e852949ad0bc04f2
Sumit-Rodge <sumitrodge27@gmail.com>
2024-07-21 21:49:57 +0530

    new vercel deployment

--- api.js
+++ api.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({
     // origin: 'http://localhost:3000',
-    origin: 'https://expense-tracker-mern-mu.vercel.app/',
+    origin: 'https://expense-tracker-ebon-three.vercel.app/',
     credentials: true
   }));
 app.use(cookieParser());
